bbfaa00e0dd35999f294a66e6f08a33ecdcc003a
SasaunkVanamali <83076992+SasaunkVanamali@users.noreply.github.com>
2022-07-24 22:47:59 +0530

    Add files via upload

---
+++ oibsip_taskno.2/main.js
@@ -0,0 +1,24 @@
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+
+let val;
+
+window.onscroll = function() {
+  if(document.documentElement.scrollTop > 20){
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  }else{
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
+}
+
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+
+let navLinks = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click" , function() {
+    navBar.classList.remove("active");
+  });
+}
